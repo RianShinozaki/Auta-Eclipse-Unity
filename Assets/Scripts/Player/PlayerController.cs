9053149769635fb74d57e2d2ee72f18b81d397c3
Rian <redkrsc@gmail.com>
2021-06-07 16:53:43 -0700

    Physics

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -20,28 +20,32 @@ public class PlayerController : PhysicsEntity
 
 
     // Start is called before the first frame update
-    public override void Start()
+    public override void Awake()
     {
-        
+        base.Awake();
     }
 
     // Update is called once per frame
     public override void Update()
     {
+        base.Update();
         NormalState();
     }
 
     public void NormalState()
     {
         if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.1f) {
-            Velocity.x = Mathf.MoveTowards(Velocity.x, Input.GetAxisRaw("Horizontal") * MoveSpeed, Accel * (AirFrictionDivide * (Grounded ? 0 : 1)));
+            Velocity.x = Mathf.MoveTowards(Velocity.x, Input.GetAxisRaw("Horizontal") * MoveSpeed, Accel * (Grounded ? 1 : AirFrictionDivide));
         } else
         {
-            Velocity.x = Mathf.MoveTowards(Velocity.x, 0, Stop * (AirFrictionDivide * (Grounded ? 0 : 1) ) );
+            Velocity.x = Mathf.MoveTowards(Velocity.x, 0, Stop * (Grounded ? 1 : AirFrictionDivide));
         }
 
         if (Input.GetButtonDown("Jump") && Grounded) {
             Velocity.y = JumpPower;
+            Debug.Log("Yump");
+            transform.position += Vector3.up * 0.1f;
+            Grounded = false;
         }
     }
 }
--- Assets/Scripts/System/PhysicsEntity.cs
+++ Assets/Scripts/System/PhysicsEntity.cs
@@ -11,16 +11,94 @@ public class PhysicsEntity : MonoBehaviour
     public float Gravity;
     public bool Grounded;
 
+    public Rigidbody2D rb;
+    public Collider2D coll;
+    public LayerMask EnvironmentMask;
+
     #endregion Physics
 
-    // Start is called before the first frame update
-    public virtual void Start()
+    public List<ContactPoint2D> GroundPoints = new List<ContactPoint2D>();
+    public List<ContactPoint2D> WallPoints = new List<ContactPoint2D>();
+    public virtual void OnCollisionStay2D(Collision2D collision)
     {
-        
+        foreach(ContactPoint2D contact in collision.contacts)
+        {
+            float angle = Vector2.SignedAngle(Vector2.up, contact.normal);
+            Debug.Log(angle);
+            if(angle >= -45 && angle <= 45)
+            {
+                GroundPoints.Add(contact);
+            } else
+            {
+                WallPoints.Add(contact);
+            }
+            
+        }
     }
+
+    public virtual void Awake()
+    {
+
+    }
+
 
     // Update is called once per frame
     public virtual void Update()
+    {
+        Vector3 vel = new Vector3(0, 0, 0);
+
+        int groundPointsNum = 0;
+        float avgAngle = 0;
+
+        foreach (ContactPoint2D contact in GroundPoints)
+        {
+            Debug.Log(Vector2.SignedAngle(Vector2.up, contact.normal));
+
+            groundPointsNum++;
+
+            avgAngle += Vector2.SignedAngle(Vector2.up, contact.normal);
+        }
+
+        if(groundPointsNum == 0 || (Grounded = false && Velocity.y > 0) )
+        {
+            Grounded = false;
+            Velocity.y -= Gravity * Time.deltaTime;
+            vel.x = Velocity.x;
+            Debug.DrawRay(transform.position, new Vector3(0, -1, 0));
+        } else
+        {
+            Grounded = true;
+            avgAngle /= groundPointsNum;
+            Velocity.y = Mathf.Sin(avgAngle * Mathf.Deg2Rad) * Velocity.x;
+            vel.x = Mathf.Cos(avgAngle * Mathf.Deg2Rad) * Velocity.x;
+        }
+
+        if (!Grounded && Velocity.y < 0) //If falling, look for collision as to land on it perfectly
+        {
+            RaycastHit2D hit;
+            hit = Physics2D.Raycast(new Vector2(transform.position.x, coll.bounds.center.y - coll.bounds.extents.y), Vector2.down, Velocity.y * Time.deltaTime, EnvironmentMask);
+            Debug.DrawRay(new Vector2(transform.position.x, coll.bounds.center.y - coll.bounds.extents.y), Vector2.down * Velocity.y * Time.deltaTime);
+
+            if (hit.collider != null) //Check for a collision
+            {
+                transform.position = hit.point + new Vector2(0, coll.bounds.extents.y); //Move to collision point
+                transform.position -= new Vector3(0, Velocity.y * Time.deltaTime, 0);
+                Velocity.y = 0;
+            }
+        }
+
+        vel.y = Velocity.y;
+        transform.position += vel * Time.deltaTime;
+
+        if (rb.IsAwake())
+        {
+            GroundPoints.Clear();
+            WallPoints.Clear();
+        }
+
+        Debug.Log("Hooray");
+    }
+    public virtual void FixedUpdate()
     {
         
     }

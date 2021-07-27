036431740d6afe1db910274fe6ddf502b2f89306
Rian <redkrsc@gmail.com>
2021-07-26 20:17:32 -0700

    Sync

--- Assets/Scripts/Items/MoneyPickup.cs
+++ Assets/Scripts/Items/MoneyPickup.cs
@@ -20,12 +20,19 @@ public class MoneyPickup : MonoBehaviour
 
     public AK.Wwise.Event PickupSound;
 
+    Rigidbody2D rb;
+
     void Start()
     {
         anim = GetComponent<Animator>();
         AnimationPlayableUtilities.PlayClip(GetComponent<Animator>(), Clips[Type], out playableGraph);
 
         opo = GetComponent<ObjectPoolObject>();
+
+        rb = GetComponent<Rigidbody2D>();
+
+        float Direction = Random.Range(Mathf.PI / 4, Mathf.PI * 3 / 4);
+        rb.velocity = new Vector2(3 * Mathf.Sin(Direction), 3 * Mathf.Cos(Direction));
     }
 
     // Update is called once per frame
